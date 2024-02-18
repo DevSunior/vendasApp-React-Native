733f178c611e2e03951680b7b4cd57401015cd06
DevSunior <devsunior@gmail.com>
2024-02-18 20:29:57 -0300

    Adding eror message to input

--- src/shared/components/input/Input.tsx
+++ src/shared/components/input/Input.tsx
@@ -6,25 +6,36 @@ import { textTypes } from "../text/textTypes";
 import { theme } from "../../themes/theme";
 
 interface InputProps extends TextInputProps {
-    title?: string
+    title?: string,
+    errorMessage?: string
 }
 
-const Input = ({title, ...props}: InputProps) => {
+const Input = ({title, errorMessage, ...props}: InputProps) => {
 
     return (
         <DisplayFlexColumn>
             {title && (
-            <Text
-            margin="0px 0px 4px 8px"
-            color={theme.colors.grayTheme.gray100}
-            type={textTypes.PARAGRAPH_SMALL_BOLD}
-            >
-                {title}
-            </Text>
+                <Text
+                margin="0px 0px 4px 8px"
+                color={theme.colors.grayTheme.gray100}
+                type={textTypes.PARAGRAPH_SMALL_BOLD}
+                >
+                    {title}
+                </Text>
             )}
             <ContainerInput
             {...props}
+            isError={!!errorMessage}
             />
+            {errorMessage && (
+                <Text
+                margin="0px 0px 0px 8px"
+                color={theme.colors.orangeTheme.orange80}
+                type={textTypes.PARAGRAPH_SMALL_BOLD}
+                >
+                    {errorMessage}
+                </Text>
+            )}
         </DisplayFlexColumn>
     )
 }

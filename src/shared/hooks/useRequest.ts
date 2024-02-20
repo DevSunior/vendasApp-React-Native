8a417511ed9ad2e4b8537412cda2ca383c719243
DevSunior <devsunior@gmail.com>
2024-02-20 20:01:27 -0300

    Adding user to hook

--- src/shared/hooks/useRequest.ts
+++ src/shared/hooks/useRequest.ts
@@ -2,21 +2,21 @@ import { useState } from "react"
 import { RequestLogin } from "../types/requestLogin";
 import { connectionAPIPost } from "../functions/connection/connectionAPI";
 import { ReturnLogin } from "../types/returnLogin";
-import { useDispatch } from "react-redux";
 import { setUserAction } from "../../store/reducers/userReducer";
+import { useUserReducer } from "../../store/reducers/userReducer/useUserReducer";
 
 
 export const useRequest = () => {
-    const dispatch = useDispatch();
+    const { setUser } = useUserReducer();
     const [loading, setLoading] = useState<boolean>(false);
     const [errorMessage, setErrorMessage] = useState<string>('');
 
     const authRequest = async (body: RequestLogin) => {
         setLoading(true);
 
-        await connectionAPIPost<ReturnLogin>('http://192.168.1.29:8080/auth', body)
+        await connectionAPIPost<ReturnLogin>('http://192.168.1.22:8080/auth', body)
         .then((result) => {
-            dispatch(setUserAction(result.user))
+            setUser(result.user)
         })
         .catch(() => {
             setErrorMessage('Usuário ou senha inválidos.')
